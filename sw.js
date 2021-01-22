654c6a04e54f2e35f791eb41c3ecae6f2f3ef176
Andres Felipe Triana Manrique <andres.010192@gmail.com>
2021-01-22 10:11:40 -0500

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -7,16 +7,16 @@ const INMUTABLE_CACHE = 'inmutable-v1'
 
 
 const APP_SHELL = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
+    // '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
     'js/sw-utils.js',
 ];
 
@@ -24,7 +24,7 @@ const APP_SHELL = [
      'https://fonts.googleapis.com/css?family=Quicksand:300,400',
  	 'https://fonts.googleapis.com/css?family=Lato:400,300', 
      'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-     '/css/animate.css',
+     'css/animate.css',
      'js/libs/jquery.js',
 
  ];
